e7f6487f9118b7d5d4f62b8e69583349a25b3b02
fgaviria <gaviria.reyes@gmail.com>
2019-01-19 20:18:03 -0500

    Finalización de my-app; basicos angular 7

---
+++ my-app/src/app/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app-header', //como se va a llamar la etiqueta
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent{
+
+}
