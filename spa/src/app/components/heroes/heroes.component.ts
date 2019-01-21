31bcc7048116ae239977c84e00ab5f13452f9ba1
fgaviria <gaviria.reyes@gmail.com>
2019-01-20 22:38:52 -0500

    Proyecto 2 SPA

---
+++ spa/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[]=[];
+
+  constructor(private _heroesService:HeroesService,
+              private router:Router) {
+
+   }
+
+   
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    //console.log(this.heroes);
+  }
+
+  verHeroe(i:number){
+    this.router.navigate(['/heroe',i])
+  }
+
+  getHeroe(i:string){
+    return this.heroes[i];
+  }
+
+}
